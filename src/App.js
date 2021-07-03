e6da944670f5ff75fabedbb873c61781986fa723
Nguyen Cong Duy <ncongduy@gmail.com>
2021-07-03 22:35:55 +0300

    redux library: matching memory game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Score } from "./features/score/Score";
+import { Board } from "./features/board/Board";
+import { useDispatch } from "react-redux";
+import { setBoard, resetCards } from "./features/board/boardSlice.js";
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+
+  const startGameHandler = () => {
+    dispatch(setBoard());
+  };
+
+  const tryAgainHandler = () => {
+    dispatch(resetCards());
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Score />
+      <Board />
+      <footer className="footer">
+        <button onClick={startGameHandler} className="start-button">
+          Start Game
+        </button>
+        <button onClick={tryAgainHandler} className="try-new-pair-button">
+          Try New Pair
+        </button>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
